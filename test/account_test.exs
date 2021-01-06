9bba8ea868b2716659972956910d81b349a31298
Adriel B <adrielbb15@gmail.com>
2021-01-06 20:27:13 -0300

    Create User and Account test

---
+++ test/account_test.exs
@@ -0,0 +1,13 @@
+
+ExUnit.start
+
+defmodule AccountTest do
+  use ExUnit.Case
+#   doctest Account
+
+  test "must register a new account" do
+    assert Account.register(
+        %User{name: 'adriel', email: 'adriel@example.com'}
+    ) == :ok
+  end
+end
